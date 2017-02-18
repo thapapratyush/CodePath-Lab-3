e26359a3f35358df3c6e0b72f141c602422ec3e1
Pratyush Thapa <pratyush@Pratyushs-MacBook-Pro.local>
2017-02-17 19:27:47 -0500

    Comitted the working project files

---
+++ GithubDemo/RepoResultsViewController.swift
@@ -0,0 +1,110 @@
+//
+//  ViewController.swift
+//  GithubDemo
+//
+//  Created by Pratyush Thapa on 5/12/15.
+//  Copyright (c) 2015 codepath. All rights reserved.
+//
+
+import UIKit
+import MBProgressHUD
+
+// Main ViewController
+class RepoResultsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
+
+
+    @IBOutlet weak var tableView: UITableView!
+    var searchBar: UISearchBar!
+    var searchSettings = GithubRepoSearchSettings()
+
+    var repos: [GithubRepo]!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.delegate = self
+        tableView.dataSource = self
+       
+        
+        // Initialize the UISearchBar
+        searchBar = UISearchBar()
+        searchBar.delegate = self
+
+        // Add SearchBar to the NavigationBar
+        searchBar.sizeToFit()
+        navigationItem.titleView = searchBar
+
+        // Perform the first search when the view controller first loads
+        doSearch()
+    }
+
+    // Perform the search.
+    fileprivate func doSearch() {
+
+        MBProgressHUD.showAdded(to: self.view, animated: true)
+
+        // Perform request to GitHub API to get the list of repositories
+        GithubRepo.fetchRepos(searchSettings, successCallback: { (newRepos) -> Void in
+
+            // Print the returned repositories to the output window
+            for repo in newRepos {
+                print(repo)
+            }
+            self.repos = newRepos
+            self.tableView.reloadData()
+            MBProgressHUD.hide(for: self.view, animated: true)
+            }, error: { (error) -> Void in
+                print(error!)
+        })
+    }
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if repos == nil{
+            return 0
+        } else {
+            return repos.count
+        }
+        
+        
+    }
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+    let cell = tableView.dequeueReusableCell(withIdentifier: "RepoCell", for: indexPath) as! RepoCell
+        
+        
+        cell.repo = repos[indexPath.row]
+        
+        return cell
+    }
+    
+    
+    }
+    
+
+    
+
+
+// SearchBar methods
+extension RepoResultsViewController: UISearchBarDelegate {
+
+    func searchBarShouldBeginEditing(_ searchBar: UISearchBar) -> Bool {
+        searchBar.setShowsCancelButton(true, animated: true)
+        return true
+    }
+
+    func searchBarShouldEndEditing(_ searchBar: UISearchBar) -> Bool {
+        searchBar.setShowsCancelButton(false, animated: true)
+        return true
+    }
+
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.text = ""
+        searchBar.resignFirstResponder()
+    }
+
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchSettings.searchString = searchBar.text
+        searchBar.resignFirstResponder()
+        doSearch()
+    }
+}
